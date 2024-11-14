0281810367d245791265b67e4e37a42991cb1408
ruandelia <ruandelia2@gmail.com>
2024-11-14 15:24:55 -0300

    criando função inserindo usuario

---
+++ includes/funcoes-usuarios.php
@@ -0,0 +1,15 @@
+<?
+ require "conecta.php";
+
+ //FunÇão para inserir novos usuários
+ function inserirUsuario($conexao, $nome, $email, $senha, $tipo){
+    // Montando o comando SQL para fazer o INSERT dos dados
+
+    $sql = "INSERT INTO usuario (nome, email, tipo, senha)
+            VALUES('$nome', '$email','$tipo', '$senha')";
+
+    // executando o comando no banco via php
+    mysqli_query($conexao, $sql);    
+
+
+}
